13c1ee4201a7c41942268fa3e9842ef7976b3efd
r-may <mayro@ethz.ch>
2016-09-13 08:56:15 +0200

    Cleanup

--- sts/happensbefore/reduce/batch_reduce.sh
+++ sts/happensbefore/reduce/batch_reduce.sh
@@ -16,14 +16,14 @@
 ./sts/happensbefore/reduce/reduce.py results/pox_eel_learningswitch-BinaryLeafTreeTopology2-steps400/hb.json &&
 ./sts/happensbefore/reduce/reduce.py results/pox_eel_learningswitch-BinaryLeafTreeTopology2-steps600/hb.json &&
 ./sts/happensbefore/reduce/reduce.py results/pox_eel_learningswitch-BinaryLeafTreeTopology2-steps800/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-StarTopology4-steps200/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-StarTopology4-steps400/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-StarTopology4-steps600/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-StarTopology4-steps800/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-StarTopology4-steps200/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-StarTopology4-steps400/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-StarTopology4-steps600/hb.json &&
-./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-StarTopology4-steps800/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-BinaryLeafTreeTopology2-steps200/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-BinaryLeafTreeTopology2-steps400/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-BinaryLeafTreeTopology2-steps600/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi-BinaryLeafTreeTopology2-steps800/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-BinaryLeafTreeTopology2-steps200/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-BinaryLeafTreeTopology2-steps400/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-BinaryLeafTreeTopology2-steps600/hb.json &&
+./sts/happensbefore/reduce/reduce.py results/pox_eel_l2_multi_fixed-BinaryLeafTreeTopology2-steps800/hb.json &&
 
 echo "FINISHED"
 
