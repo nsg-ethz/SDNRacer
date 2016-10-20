0d769a67539807fc95378c775a5ba4ca402889b9
r-may <mayro@ethz.ch>
2016-10-20 08:49:04 +0200

    modified config

--- sts/happensbefore/reduce/tools/batch_cluster_only.sh
+++ sts/happensbefore/reduce/tools/batch_cluster_only.sh
@@ -41,7 +41,7 @@ only_string="floodlight_loadbalancer"
 
 # Process the following number of steps
 steps[0]="800"
-#steps[1]="400"
+steps[1]="1000"
 #steps[2]="600"
 #steps[3]="800"
 #steps[4]="1000"
