f3bfb7ae8c8815ca9d802c3a018e694047837600
r-may <mayro@ethz.ch>
2016-10-23 19:34:22 +0200

    Added N/A in output for missing traces/results

--- sts/happensbefore/reduce/tools/eval_v3.py
+++ sts/happensbefore/reduce/tools/eval_v3.py
@@ -46,6 +46,9 @@ class Evaluation:
                    'learningswitch': 'LearningSwitch',
                    '_fixed': ' Fx'}
 
+    # Expected simulations number of steps
+    self.exp_steps = [200, 400, 600, 800, 1000]
+
   def run(self):
     # Fetch data
     self.fetch_data()
@@ -63,121 +66,116 @@ class Evaluation:
       for controller in sorted(self.eval_dicts.keys()):
         if controller not in self.median:
           self.median[controller] = {}
-        # Separate controller and module
-        if controller.startswith('floodlight'):
-          app = controller.replace('floodlight_', '')
-          controller_str = 'Floodlight'
-        elif controller.startswith('pox_eel'):
-          app = controller.replace('pox_eel_', '')
-          controller_str = 'POX EEL'
-        else:
-          raise RuntimeError("Unknown controller string %s" % controller)
+        for app in sorted(self.eval_dicts[controller].keys()):
+          if app not in self.median[controller]:
+            self.median[controller][app] = {}
+          for topology in sorted(self.eval_dicts[controller][app].keys()):
+            if topology not in self.median[controller][app]:
+              self.median[controller][app][topology] = {}
+            for steps in sorted(self.eval_dicts[controller][app][topology].keys()):
+              if steps not in self.median[controller][app][topology]:
+                self.median[controller][app][topology][steps] = {'n_events': [],
+                                                                 'n_graphs': [],
+                                                                 'n_iso': [],
+                                                                 'n_iso_timeout': [],
+                                                                 'n_iso_total': [],
+                                                                 'n_final': [],
+                                                                 't_t': [],
+                                                                 't_hb': [],
+                                                                 't_p': [],
+                                                                 't_s': [],
+                                                                 't_i': [],
+                                                                 't_dm': [],
+                                                                 't_sim': [],
+                                                                 'num': 0}
 
-        for topology in sorted(self.eval_dicts[controller].keys()):
-          if topology not in self.median[controller]:
-            self.median[controller][topology] = {}
-          for steps in sorted(self.eval_dicts[controller][topology].keys()):
-            if steps not in self.median[controller][topology]:
-              self.median[controller][topology][steps] = {'controller_str': controller_str,
-                                                          'app': app,
-                                                          'topology': topology,
-                                                          'steps': steps,
-                                                          'n_events': [],
-                                                          'n_graphs': [],
-                                                          'n_iso': [],
-                                                          'n_iso_timeout': [],
-                                                          'n_iso_total': [],
-                                                          'n_final': [],
-                                                          't_t': [],
-                                                          't_hb': [],
-                                                          't_p': [],
-                                                          't_s': [],
-                                                          't_i': [],
-                                                          't_dm': [],
-                                                          't_c': [],
-                                                          't_sim': [],
-                                                          'num': 0}
+              for i, data in sorted(self.eval_dicts[controller][app][topology][steps].iteritems()):
+                # Add data for median
+                self.median[controller][app][topology][steps]['num'] += 1
+                self.median[controller][app][topology][steps]['n_events'].append(int(data['info']['Number of events']))
+                self.median[controller][app][topology][steps]['n_graphs'].append(data['info']['Number of graphs'])
+                self.median[controller][app][topology][steps]['n_iso'].append(
+                    data['clustering']['info']['Number of clusters after iso'])
+                self.median[controller][app][topology][steps]['n_iso_timeout'].append(data['clustering']['iso init timeout'])
+                self.median[controller][app][topology][steps]['n_iso_total'].append(data['clustering']['iso init total'])
+                self.median[controller][app][topology][steps]['n_final'].append(data['info']['Number of clusters'])
+                self.median[controller][app][topology][steps]['t_t'].append(data['time']['total'])
+                self.median[controller][app][topology][steps]['t_hb'].append(data['time']['hb_graph'])
+                if data['sim_time'] is not None:
+                  self.median[controller][app][topology][steps]['t_sim'].append(data['sim_time'])
 
-            for i, data in sorted(self.eval_dicts[controller][topology][steps].iteritems()):
-              # Add data for median
-              self.median[controller][topology][steps]['num'] += 1
-              self.median[controller][topology][steps]['n_events'].append(int(data['info']['Number of events']))
-              self.median[controller][topology][steps]['n_graphs'].append(data['info']['Number of graphs'])
-              self.median[controller][topology][steps]['n_iso'].append(
-                  data['clustering']['info']['Number of clusters after iso'])
-              self.median[controller][topology][steps]['n_iso_timeout'].append(data['clustering']['iso init timeout'])
-              self.median[controller][topology][steps]['n_iso_total'].append(data['clustering']['iso init total'])
-              self.median[controller][topology][steps]['n_final'].append(data['info']['Number of clusters'])
-              self.median[controller][topology][steps]['t_t'].append(data['time']['total'])
-              self.median[controller][topology][steps]['t_hb'].append(data['time']['hb_graph'])
-              if data['sim_time'] is not None:
-                self.median[controller][topology][steps]['t_sim'].append(data['sim_time'])
-
-              # Generate output
-              line = ""
-              line += "%s," % controller_str
-              line += "%s," % app
-              line += "%s," % topology
-              line += "%s," % str(steps)
-              line += "%s," % str(i)  # Iteration number
-              line += "%s," % data['info']['Number of events']
-              line += "%s," % data['info']['Number of graphs']
-              line += "%s," % data['clustering']['info']['Number of clusters after iso']
-              line += "%s," % data['info']['Number of clusters']
-              line += "%.3f s," % data['time']['total']
-              line += "%.3f s," % data['time']['hb_graph']
-              line += "%.3f s," % data['preprocessor']['time']['Total']
-              line += "%.3f s," % data['subgraph']['time']['Total']
-              line += "%.3f s," % data['clustering']['time']['Initialize cluster']
-              line += "%.3f s," % data['clustering']['time']['Calculate distance matrix']
-              line += "%.3f s," % \
-                      (data['clustering']['time']['Calculate clustering'] +
-                       data['clustering']['time']['Assign new clusters'])
-              if data['sim_time'] is not None:
-                line += "%.3f s" % data['sim_time']
-              else:
-                line += "N/A"
-              line += "\n"
-              f.write(line)
+                # Generate output
+                line = ""
+                line += "%s," % controller
+                line += "%s," % app
+                line += "%s," % topology
+                line += "%s," % str(steps)
+                line += "%s," % str(i)  # Iteration number
+                line += "%s," % data['info']['Number of events']
+                line += "%s," % data['info']['Number of graphs']
+                line += "%s," % data['clustering']['info']['Number of clusters after iso']
+                line += "%s," % data['info']['Number of clusters']
+                line += "%.3f s," % data['time']['total']
+                line += "%.3f s," % data['time']['hb_graph']
+                line += "%.3f s," % data['preprocessor']['time']['Total']
+                line += "%.3f s," % data['subgraph']['time']['Total']
+                line += "%.3f s," % data['clustering']['time']['Initialize cluster']
+                line += "%.3f s," % data['clustering']['time']['Calculate distance matrix']
+                line += "%.3f s," % \
+                        (data['clustering']['time']['Calculate clustering'] +
+                         data['clustering']['time']['Assign new clusters'])
+                if data['sim_time'] is not None:
+                  line += "%.3f s" % data['sim_time']
+                else:
+                  line += "N/A"
+                line += "\n"
+                f.write(line)
 
     with open(self.median_file, 'w') as f:
       # Average
       f.write("Controller,App,Topology,Steps,Number,# Events,# Races,# Isomorphic Clusters,# Timeouts,"
               "# Final Clusters,Total Time,Hb_Graph,Sim Time\n")
       for controller in sorted(self.median.keys()):
-        for topology in sorted(self.median[controller].keys()):
-          for steps, data in sorted(self.median[controller][topology].iteritems()):
-            line = ""
-            line += "%s," % data['controller_str']
-            line += "%s," % data['app']
-            line += "%s," % data['topology']
-            line += "%s," % data['steps']
-            line += "%s," % data['num']
-            line += "%d," % np.median(data['n_events'])
-            line += "%d," % np.median(data['n_graphs'])
-            line += "%d (%.2f %%)," % (int(np.median(data['n_iso'])),
-                                       round(float(np.median(data['n_iso'])) /
-                                       float(np.median(data['n_graphs'])) * 100, 2))
-            if np.median(data['n_iso_total']) == 0:
-              assert np.median(data['n_iso_timeout']) == 0, 'More timeouts than total'
-              line += "0 (0.00%%),"
-            else:
-              line += "%d (%.2f %%)," % (int(np.median(data['n_iso_timeout'])),
-                                         round(float(np.median(data['n_iso_timeout'])) /
-                                         float(np.median(data['n_iso_total'])) * 100, 2))
+        for app in sorted(self.median[controller].keys()):
+          for topology in sorted(self.median[controller][app].keys()):
+            for steps in self.exp_steps:
+              line = ""
+              line += "%s," % controller
+              line += "%s," % app
+              line += "%s," % topology
+              line += "%s," % steps
 
-            line += "%d (%.2f %%)," % (int(np.median(data['n_final'])),
-                                       round(float(np.median(data['n_final'])) /
-                                       float(np.median(data['n_graphs'])) * 100, 2))
-            line += "%.3f s," % round(np.median(data['t_t']), 3)
-            line += "%.3f s," % round(np.median(data['t_hb']), 3)
-            if data['t_sim']:
-              line += "%.1f s" % np.median(data['t_sim'])
-            else:
-              line += "N/A"
-            line += "\n"
+              # Fill line with N/A if no data is available
+              if steps in self.median[controller][app][topology]:
+                data = self.median[controller][app][topology][steps]
+                line += "%s," % data['num']
+                line += "%d," % np.median(data['n_events'])
+                line += "%d," % np.median(data['n_graphs'])
+                line += "%d (%.2f %%)," % (int(np.median(data['n_iso'])),
+                                           round(float(np.median(data['n_iso'])) /
+                                           float(np.median(data['n_graphs'])) * 100, 2))
+                if np.median(data['n_iso_total']) == 0:
+                  assert np.median(data['n_iso_timeout']) == 0, 'More timeouts than total'
+                  line += "0 (0.00%%),"
+                else:
+                  line += "%d (%.2f %%)," % (int(np.median(data['n_iso_timeout'])),
+                                             round(float(np.median(data['n_iso_timeout'])) /
+                                             float(np.median(data['n_iso_total'])) * 100, 2))
 
-            f.write(line)
+                line += "%d (%.2f %%)," % (int(np.median(data['n_final'])),
+                                           round(float(np.median(data['n_final'])) /
+                                           float(np.median(data['n_graphs'])) * 100, 2))
+                line += "%.3f s," % round(np.median(data['t_t']), 3)
+                line += "%.3f s," % round(np.median(data['t_hb']), 3)
+                if data['t_sim']:
+                  line += "%.1f s" % np.median(data['t_sim'])
+                else:
+                  line += "N/A"
+                line += "\n"
+              else:
+                line += "N/A,N/A,N/A,N/A,N/A,N/A,N/A,N/A,N/A\n"
+
+              f.write(line)
 
   def fetch_data(self):
     for folder in os.listdir(self.eval_folder):
@@ -206,18 +204,29 @@ class Evaluation:
 
       settings = trace_info.split('-')
       controller = self.rename(settings[0])
+      # Separate controller and module
+      if controller.startswith('floodlight'):
+        app = controller.replace('floodlight_', '')
+        controller = 'Floodlight'
+      elif controller.startswith('pox_eel'):
+        app = controller.replace('pox_eel_', '')
+        controller = 'POX EEL'
+      else:
+        raise RuntimeError("Unknown controller string %s" % controller)
       topology = self.rename(settings[1])
       steps = int(re.search(r'\d+$', settings[2]).group())
       iteration = settings[3]
 
       if controller not in self.eval_dicts:
         self.eval_dicts[controller] = {}
-      if topology not in self.eval_dicts[controller]:
-        self.eval_dicts[controller][topology] = {}
-      if steps not in self.eval_dicts[controller][topology]:
-        self.eval_dicts[controller][topology][steps] = {}
-      self.eval_dicts[controller][topology][steps][iteration] = data
-      self.eval_dicts[controller][topology][steps][iteration]['sim_time'] = None
+      if app not in self.eval_dicts[controller]:
+        self.eval_dicts[controller][app] = {}
+      if topology not in self.eval_dicts[controller][app]:
+        self.eval_dicts[controller][app][topology] = {}
+      if steps not in self.eval_dicts[controller][app][topology]:
+        self.eval_dicts[controller][app][topology][steps] = {}
+      self.eval_dicts[controller][app][topology][steps][iteration] = data
+      self.eval_dicts[controller][app][topology][steps][iteration]['sim_time'] = None
 
   def fetch_simulation_time(self):
     with open(self.sim_log, 'r') as f:
@@ -229,13 +238,23 @@ class Evaluation:
           continue
         settings = (l[0].split("/")[-1]).split('-')
         controller = self.rename(settings[0])
+        # Separate controller and module
+        if controller.startswith('floodlight'):
+          app = controller.replace('floodlight_', '')
+          controller = 'Floodlight'
+        elif controller.startswith('pox_eel'):
+          app = controller.replace('pox_eel_', '')
+          controller = 'POX EEL'
+        else:
+          raise RuntimeError("Unknown controller string %s" % controller)
         topology = self.rename(settings[1])
         steps = int(re.search(r'\d+$', settings[2]).group())
         iteration = settings[3]
         try:
-          self.eval_dicts[controller][topology][steps][iteration]['sim_time'] = float(l[2])
+          self.eval_dicts[controller][app][topology][steps][iteration]['sim_time'] = float(l[2])
         except KeyError:
-          print "Not in eval dicts: %s" % line
+          # print "Not in eval dicts: %s" % line
+          pass
 
   def rename(self, s):
     for k, v in self.r_dict.iteritems():
